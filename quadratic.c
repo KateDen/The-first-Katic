77fc323795e0c65c56d4747f1081268a874e360a
KateDen <89272804+KateDen@users.noreply.github.com>
2021-08-23 00:12:33 +0300

    Update quadratic.c

--- quadratic.c
+++ quadratic.c
@@ -1,54 +1,126 @@
 #include "stdio.h"
-#include "cmath"
+#include "math.h"
 
- main ()
+#define NONQUAD 1;
+#define NO_SOL 2;
+#define ONE_SOL 3;
+#define TWO_SOL 4;
+#define EXTRA 5;
+
+int scan_variables (double a, double b, double c);
+int solve_equation (double * coefa, double * coefb, double * coefc);
+int print_answer (int * answerprint, double * xprint);
+
+int main ()
 {
-    float a, b, c, D, x1, x2, x0;
 
-    //разъ€сн€ющие слова дл€ юзера
+    double a, b, c;
+    a = 0;
+    b = 0;
+    c = 0;
+
+    scan_variables (a, b, c);
+
+    return 0;
+}
+
+int scan_variables (double a, double b, double c)
+{
 
     printf ("This program can solve any of your quadratic equations!\n");
     printf ("The equation has the form: a*x^2+b*x+c=0.\n");
     printf ("Please enter the values of your coefficients.\n");
     printf ("(If any of them is zero or negative, enter 0 or the minus sign before the number).\n");
     printf ("a=");
-    scanf ("%f", &a);
+    scanf ("%lf", &a);
     printf ("b=");
-    scanf ("%f", &b);
+    scanf ("%lf", &b);
     printf ("c=");
-    scanf ("%f", &c);
+    scanf ("%lf", &c);
 
-    if (a == 0)     //проверка на зануление коэффициента при х^2
+    solve_equation (&a, &b, &c);
+
+    return 0;
+}
+
+int solve_equation (double * coefa, double * coefb, double * coefc)
+{
+    double D;
+    D = 0;
+    int answer = 0;
+
+    double epsilon = 0.00001;
+
+    if (fabs (*coefa) < epsilon)
     {
-        x0 = (-1.0)*c/b;
-        printf ("Your equation has one solution: x= %1.2f\n", x0);
+        double x0 = 0;
+        x0 = (-1.0)*(*coefc)/(*coefb);
+        answer = NONQUAD;
+        print_answer (&answer, &x0);
     }
 
     else
     {
-        D = b*b - 4.0*a*c;       //вычисление дискриминанта
+        D = pow (*coefb,2) - 4.0*(*coefa)*(*coefc);
 
-        //сравнение D с нулЄм
         if (D < 0)
         {
-            printf ("Your quadratic equation has no solutions.\n");
+            answer = NO_SOL;
+            double nosol = 0.0;
+            print_answer (&answer, &nosol);
         }
 
-        else if (D == 0)
+        else if (fabs (D) < epsilon)
         {
-            x0 = (-b)/(2.0*a);
-            printf ("Your quadratic equation has one solution: x= %1.2f\n", x0);
+            double x = 0;
+            x = (-(*coefb))/(2.0*(*coefa));
+            answer = ONE_SOL;
+            print_answer (&answer, &x);
         }
 
         else
         {
-            x1 = (-b + sqrt((double)D))/(2.0*a);
-            x2 = (-b - sqrt((double)D))/(2.0*a);
+            double x1 = 0, x2 = 0;
+            x1 = ((-1.0)*(*coefb) + sqrt((double)D))/(2.0*(*coefa));
+            x2 = ((-1.0)*(*coefb) - sqrt((double)D))/(2.0*(*coefa));
 
-            printf ("Your quadratic equation has two solutions:\nx1= %1.2f\n", x1);
-            printf ("x2= %1.2f\n", x2);
+            answer = TWO_SOL;
+            print_answer (&answer, &x1);
+            answer = EXTRA;
+            print_answer (&answer, &x2);
         }
-    return 0;
+
+        return 0;
     }
 }
 
+int print_answer (int * answerprint, double * xprint)
+{
+
+    if (*answerprint == 1)
+    {
+        printf ("Your equation has only one solution: x= %1.2lf\n", *xprint);
+    }
+
+    if (*answerprint == 2)
+    {
+        printf ("Your quadratic equation has no solutions.\n");
+    }
+
+    if (*answerprint == 3)
+    {
+        printf ("Your quadratic equation has one solution: x= %1.2lf\n", *xprint);
+    }
+
+    if (*answerprint == 4)
+    {
+        printf ("Your quadratic equation has two solutions:\nx1= %1.2lf\n", *xprint);
+    }
+
+    if (*answerprint == 5)
+    {
+        printf ("x2= %1.2lf\n", *xprint);
+    }
+
+    return 0;
+}
