9add7d2c0958ee5e2f12f2d797cf08dc54ab807a
KateDen <89272804+KateDen@users.noreply.github.com>
2021-08-27 21:04:43 +0300

    Update quadratic.c

--- quadratic.c
+++ quadratic.c
@@ -1,85 +1,106 @@
 /*!
 	\brief  Quadratic equation solver.
-	\author Katik
+	\author Kate_Den
 	\version 1.0
 	\date 25.08.2021
 
-	This program can solve any of your quadratic equations.
+	This program can solve any of your quadratic equations of the form ax^2 + bx + c = 0.
 */
+
 
 #include <stdio.h>
 #include <math.h>
+#include <assert.h>
+//#include "C:\projects\quadration_tests.c"
 
-enum answertags {  /// Constants for different numbers of quadratic equation solutions
-     NO_SOL,    ///< For the case, when equation has no solutions
-    NONQUAD,    ///< For the case, when equation has only one solution and the coefficien behind x^2 is zero
-    ONE_SOL,    ///< For the case, when equation has one solution
-    TWO_SOL,    ///< For the case, when equation has two solutions
-    INFINITY_SOL    ///< For the case when equation has an infinite number of solutions
-};
+/// Constants for different state of equation solutions
+enum answertags
+    {
+     NO_SOL,          ///< no solutions
+     ONE_SOL_NONQUAD, ///< only one solution of a non quadration eqiation
+     ONE_SOL,         ///< one solution
+     TWO_SOL,         ///< two solutions
+     INFINITY_SOL     ///< an infinite number of solutions
+    };
 
 
 /*!
 Displays the explanatory notes and scans the coefficients
-\param a,b,c coeffitients of a quadartic equation
+\param [in] a, b, c coeffitients of a quadartic equation
 */
 int scan_variables (double* a, double* b, double* c);
 
 
 /*!
-Checks if a variable is zero
-\param x gets the variable
+Checks if a double variable is zero
+\param [in] x gets the variable
 \return true or false for comparison with zero
 */
 bool is_zero (double x);
 
 
 /*!
-Prints answer(s) for all cases or says about mistakes
-\param answer shows the amount of solutions
-\param xprint1, xprint2 get variable assignments
+Prints answer(s) for all cases
+\param [in] answer shows the state of solution
+\param [in] xprint1, xprint2 get variable assignments
 */
-void print_answer (answertags answer, double* xprint1, double* xprint2);
+void print_answer (answertags answer, double xprint1, double xprint2);
 
 /*!
-Gets the coefficients assignments and solves the equation for all cases
-\param coefa,coefb,coefc coeffitients of a quadartic equation
-\param x1, x2 get solutions
+Solves the linear equation and checks coefficients b and c
+\param [in] coefb, coefc gets the coefficients' values
+\param [out] x gets the solution
+\return
+*/
+answertags solve_linear_equations (double coefb, double coefc, double* x);
+
+
+/*!
+Gets the coefficients' values and solves the equation for all cases
+\param [in] coefa, coefb, coefc coefficients of a quadartic equation
+\param [out] x1, x2 get solutions
 \return answer different constants for different cases
 */
-enum answertags solve_equation (double coefa, double coefb, double coefc, double* x1, double* x2);
+answertags solve_quad_equation (double coefa, double coefb, double coefc, double* x1, double* x2);
 
+#include "quadration_tests.h"
 /*!
-Calls other functions and warns about errors
+Displays the solver
 */
 int main ()
 {
-    double a = 0.5, b = 0.5, c = 0.5, x1 = 0, x2 = 0;   ///< a, b, c first, second and third coefficients of the quadratic equation respectively
+    TEST ();
 
-    int error = scan_variables (&a, &b, &c);                  ///< callin the function to enter the coefficients values
-    if (error == 1)                                           ///< checking the mistakes
+    double a = 0, b = 0, c = 0, x1 = 0, x2 = 0;   /// a, b, c first, second and third coefficients of the quadratic equation respectively
+
+    int error = scan_variables (&a, &b, &c);
+    if (error == 1)
     {
-        printf ("You have entered wrong symbols.\n");         ///<warning for wrong symbol entered
+        printf ("You have entered wrong symbols.\n");
         return 0;
     }
 
-    enum answertags ret = solve_equation (a, b, c, &x1, &x2);  ///< callingthe function to give the coefficients values and get the answer(s)
-    print_answer (ret, &x1, &x2);                              ///< callin the function to print the answer(s) or warnings
+    answertags ret = solve_quad_equation (a, b, c, &x1, &x2);  /// ret gets returned values
+    print_answer (ret, x1, x2);
 
     return 0;
 }
 
 int scan_variables (double* a, double* b, double* c)
 {
-    ///< explanatory notes
+    assert (a != NULL);
+    assert (b != NULL);
+    assert (c != NULL);
+    assert (isfinite(*a));
+    assert (isfinite(*b));
+    assert (isfinite(*c));
+
     printf ("This program can solve any of your quadratic equations!\n");
     printf ("The equation has the form: a*x^2+b*x+c=0.\n");
-    printf ("Please enter the values of your coefficients.\n");
-    printf ("(If any of them is zero or negative, enter 0 or the minus sign before the number).\n\n");
+    printf ("Please enter the values of your coefficients.\n\n");
 
     printf ("Enter coefficients a, b, c: \n");
 
-    ///< wrong entered errors check
     if (3 != scanf ("%lg %lg %lg", a, b, c))
     {
         return 1;
@@ -90,55 +111,73 @@ int scan_variables (double* a, double* b, double* c)
 
 bool is_zero (double x)
 {
-    const double epsilon = 1e-5;  ///< the constant close to zero
+    const double epsilon = 1e-2;
     return fabs(x) < epsilon;
 }
 
-enum answertags solve_equation (double coefa, double coefb, double coefc, double* x1, double* x2)
+answertags solve_quad_equation (double coefa, double coefb, double coefc, double* x1, double* x2)
 {
-    double D = 0;     ///< the variable for Discriminant values
+    assert (x1);
+    assert (x2);
+    assert (isfinite(coefa));
+    assert (isfinite(coefb));
+    assert (isfinite(coefc));
 
-    if (is_zero(coefa) && is_zero(coefb) && is_zero(coefc))  ///< checking if the equation has an infinite number of solutions
+    if (is_zero (coefa))
+    {
+        return solve_linear_equations (coefb, coefc, x1);
+    }
+
+    double D = pow (coefb,2) - 4.0*(coefa)*(coefc);                 /// Discriminant formula
+
+    if (D < 0)
+    {
+        *x1 = 0;
+        *x2 = 0;
+        return NO_SOL;
+    }
+
+    if (is_zero(D))
+    {
+        *x1 = (-(coefb)) / (2.0*(coefa));
+        *x2 = 0;
+        return ONE_SOL;
+    }
+
+    *x1 = ((-1.0)*(coefb) + sqrt((double)D)) / (2.0*(coefa));   /// formulas for two solutions
+    *x2 = ((-1.0)*(coefb) - sqrt((double)D)) / (2.0*(coefa));
+    return TWO_SOL;
+}
+
+answertags solve_linear_equations (double coefb, double coefc, double* x)
+{
+    assert (isfinite(coefb));
+    assert (isfinite(coefc));
+
+    if (is_zero(coefb) && is_zero(coefc))
     {
         return INFINITY_SOL;
     }
 
-    if (is_zero(coefa) && is_zero(coefb))                    ///< checking if the equation has no solutions due to x absence
+    if (is_zero(coefb))
     {
-        return NO_SOL;
-    }
-                                                             ///< checking if the first coefficient is zero so the equation has onesolution
-    if (is_zero(coefa))
-    {
-        *x1 = (-(coefc))/(coefb);
-        return NONQUAD;
-    }
-
-    D = pow (coefb,2) - 4.0*(coefa)*(coefc);                 ///< Discriminant formula
-
-    if (D < 0)                                               ///< checking if the equation has no solutions due to negative Discriminant
-    {
+        *x = 0;
         return NO_SOL;
     }
 
-    if (is_zero(D))                                          ///< checking if the equation has no solutions due to Discriminant is zero
-    {
-        *x1 = (-(coefb))/(2.0*(coefa));
-        return ONE_SOL;
-    }
-
-    *x1 = ((-1.0)*(coefb) + sqrt((double)D))/(2.0*(coefa));   ///< formulas for two solutions
-    *x2 = ((-1.0)*(coefb) - sqrt((double)D))/(2.0*(coefa));
-    return TWO_SOL;
+    *x = (-(coefc))/(coefb);
+    return ONE_SOL_NONQUAD;
 }
 
-void print_answer (enum answertags answer, double* xprint1, double* xprint2)
+void print_answer (answertags answer, double xprint1, double xprint2)
 {
-    ///< printing for every case of solutions
+    assert (isfinite(xprint1));
+    assert (isfinite(xprint1));
+
     switch (answer)
     {
-        case NONQUAD:
-            printf ("Your equation has only one solution: x = %lf\n", *xprint1);
+        case ONE_SOL_NONQUAD:
+            printf ("Your equation has only one solution: x = %.2lf\n", xprint1);
             break;
 
         case NO_SOL:
@@ -146,12 +185,12 @@ void print_answer (enum answertags answer, double* xprint1, double* xprint2)
             break;
 
         case ONE_SOL:
-            printf ("Your quadratic equation has one solution: x = %.2lf\n", *xprint1);
+            printf ("Your quadratic equation has one solution: x = %.2lf\n", xprint1);
             break;
 
         case TWO_SOL:
             printf ("Your quadratic equation has two solutions:\nx1 = %.2lf\nx2 = %.2lf\n",
-                *xprint1, *xprint2);
+                    xprint1, xprint2);
             break;
 
         case INFINITY_SOL:
@@ -159,6 +198,6 @@ void print_answer (enum answertags answer, double* xprint1, double* xprint2)
             break;
 
         default:
-            printf ("Unexpected error");    ///< warning about an unexpected error
+            printf ("Unexpected error");
     }
 }
